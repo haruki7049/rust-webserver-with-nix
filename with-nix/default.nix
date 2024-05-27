38734e331258c5b17f197170efe0e898116fb36c
haruki7049 <tontonkirikiri@gmail.com>
2024-05-27 13:01:36 +0900

    chore: Edit default.nix

--- with-nix/default.nix
+++ with-nix/default.nix
@@ -1,5 +1,5 @@
 { rust_overlay ? import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/8ef3f6a8f5af867ab5f75fc86fbd934a6351820b.tar.gz)
-, crane ? import (builtins.fetchTarball https://github.com/ipetkov/crane/archive/19ca94ec2d288de334ae932107816b4a97736cd8.tar.gz)
+, crane ? import (builtins.fetchTarball https://github.com/ipetkov/crane/archive/19ca94ec2d288de334ae932107816b4a97736cd8.tar.gz) { }
 , pkgs ? import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/057f9aecfb71c4437d2b27d3323df7f93c010b7e.tar.gz) {
     overlays = [
       rust_overlay
@@ -8,7 +8,7 @@
 , rust ? pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
 }:
 
-craneLib.buildPackage rec {
+crane.buildPackage rec {
   src = ./.;
   doCheck = true;
 }
