9b2b2121684578c1cedd0bfba00deba7152803d8
haruki7049 <tontonkirikiri@gmail.com>
2024-05-27 12:13:23 +0900

    chore: Init Server with-flakes version

--- with-nix-flakes/src/main.rs
+++ with-nix-flakes/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router, routing::get,
+};
+
+const IP_ADDRESS: &str = "127.0.0.1:8888";
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind(IP_ADDRESS).await.unwrap();
+
+    println!("Server is up on {}...", IP_ADDRESS);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "Hello, with Nix-flakes!!"
 }
